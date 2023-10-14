78e8e28c0ed3e5e7d78e525ac547c96d55372c6e
Everton Russomanno <evertonmartins154@gmail.com>
2023-10-14 17:05:34 -0300

    Add initial project

---
+++ src/main/java/br/com/evertonmartins/springbootmongodb/SpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.evertonmartins.springbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongodbApplication.class, args);
+	}
+
+}
