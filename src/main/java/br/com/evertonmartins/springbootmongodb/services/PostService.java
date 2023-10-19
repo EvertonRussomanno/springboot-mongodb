b1a620d9f1a245e1ab1bd88b754b263ec0913640
Everton Russomanno <evertonmartins154@gmail.com>
2023-10-18 23:35:17 -0300

    Add multi @Query

--- src/main/java/br/com/evertonmartins/springbootmongodb/services/PostService.java
+++ src/main/java/br/com/evertonmartins/springbootmongodb/services/PostService.java
@@ -3,6 +3,7 @@ package br.com.evertonmartins.springbootmongodb.services;
 import br.com.evertonmartins.springbootmongodb.domain.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -11,5 +12,7 @@ public interface PostService {
     Post findById(String id);
 
     List<Post> findByTitle(String text);
+
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
--- src/main/java/br/com/evertonmartins/springbootmongodb/services/impl/PostServiceImpl.java
+++ src/main/java/br/com/evertonmartins/springbootmongodb/services/impl/PostServiceImpl.java
@@ -7,6 +7,7 @@ import br.com.evertonmartins.springbootmongodb.services.exception.ObjectNotFound
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,5 +27,11 @@ public class PostServiceImpl implements PostService {
     public List<Post> findByTitle(String text) {
         return postRepository.searchTitle(text);
 //        return postRepository.findByTitleContainingIgnoreCase(text);
+    }
+
+    @Override
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return postRepository.fullSearch(text, minDate, maxDate);
     }
 }
