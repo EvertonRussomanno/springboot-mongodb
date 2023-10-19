6920e76b4b27244131eafa6787a16dd04d5504ed
Everton Russomanno <evertonmartins154@gmail.com>
2023-10-18 23:02:15 -0300

    Add simple @Query

--- src/main/java/br/com/evertonmartins/springbootmongodb/services/impl/PostServiceImpl.java
+++ src/main/java/br/com/evertonmartins/springbootmongodb/services/impl/PostServiceImpl.java
@@ -24,6 +24,7 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public List<Post> findByTitle(String text) {
-        return postRepository.findByTitleContainingIgnoreCase(text);
+        return postRepository.searchTitle(text);
+//        return postRepository.findByTitleContainingIgnoreCase(text);
     }
 }
